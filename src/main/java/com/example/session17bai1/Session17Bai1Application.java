1647ae76dd565ad715eeb7212adc106e2d89acff
thaominh2006 <dothaominh2401@gmail.com>
2026-09-24 21:21:03 +0700

    Update Session17Bai1Application.java

--- src/main/java/com/example/session17bai1/Session17Bai1Application.java
+++ src/main/java/com/example/session17bai1/Session17Bai1Application.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-/**
- * Lớp khởi chạy chính của ứng dụng.
- * Annotation {@link EnableCaching} kích hoạt cơ chế Spring Cache (AOP proxy cho @Cacheable, @CacheEvict...).
- */
-@SpringBootApplication
-@EnableCaching
 public class Session17Bai1Application {
 
     public static void main(String[] args) {
